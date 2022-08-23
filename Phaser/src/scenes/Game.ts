42e9ba702fd84b593282a356377ebecaff0fa22a
Jason Tsorlinis <jtsorlinis@gmail.com>
2022-08-23 16:43:05 +1000

    Added particle version of phaser

--- Phaser/src/scenes/Game.ts
+++ Phaser/src/scenes/Game.ts
@@ -1,11 +1,16 @@
 import Phaser from "phaser";
-import Bunny from "./Bunny";
 
+const gravity = 30;
+const minX = 0;
+const maxX = 800;
+const minY = 0;
+const maxY = 600;
 export default class Demo extends Phaser.Scene {
   startBunnyCount = 10;
   count = 0;
   counter!: Phaser.GameObjects.Text;
   fps!: Phaser.GameObjects.Text;
+  emitter!: Phaser.GameObjects.Particles.ParticleEmitter;
   constructor() {
     super("GameScene");
   }
@@ -23,24 +28,52 @@ export default class Demo extends Phaser.Scene {
     this.counter = this.add.text(5, 25, `Bunnies: ${this.count}`).setDepth(2);
     this.add.rectangle(0, 0, 150, 45, 0x000000).setOrigin(0, 0).setDepth(1);
 
-    // Bunnies
-    for (var i = 0; i < this.startBunnyCount; i++) {
-      new Bunny(this);
-    }
+    const particles = this.add.particles("bunny");
+    this.emitter = particles.createEmitter({
+      active: true,
+      frequency: -1,
+      x: 10,
+      y: 10,
+      lifespan: Number.MAX_VALUE,
+    });
   }
 
   update(time: number, delta: number): void {
     this.fps.text = `FPS: ${this.game.loop.actualFps.toFixed(1)}`;
+    this.emitter.forEachAlive((bunny) => {
+      bunny.velocityY += gravity;
 
-    // Add bunnies while over 59fps
-    if ((1 / delta) * 1000 > 59) {
-      if (this.count < 200000) {
-        for (var i = 0; i < 100; i++) {
-          new Bunny(this);
-          this.count++;
-        }
+      if (bunny.x > maxX) {
+        bunny.velocityX *= -1;
+        bunny.x = maxX;
+      } else if (bunny.x < minX) {
+        bunny.velocityX *= -1;
+        bunny.x = minX;
       }
-      this.counter.text = `Bunnies: ${this.count}`;
+
+      if (bunny.y > maxY) {
+        bunny.velocityY *= -0.85;
+        bunny.y = maxY;
+        bunny.rotation = (Math.random() - 0.5) * 0.2;
+        if (Math.random() > 0.5) {
+          bunny.velocityY -= Math.random() * 360;
+        }
+      } else if (bunny.y < minY) {
+        bunny.velocityY = 0;
+        bunny.y = minY;
+      }
+    }, this);
+
+    // // Add bunnies while over 59fps
+    if ((1 / delta) * 1000 > 59) {
+      // if (this.emitter.getParticleCount() < 50) {
+      for (var i = 0; i < 100; i++) {
+        const particle = this.emitter.emitParticle(1, 10, 10);
+        particle.velocityX = Math.random() * 600;
+        particle.velocityY = Math.random() * 600 - 300;
+      }
     }
+
+    this.counter.text = `Bunnies: ${this.emitter.getParticleCount()}`;
   }
 }
