19b7046eced0e38a5070cfe343e65e25c1119925
jtsorlinis <jtsorlinis@gmail.com>
2022-09-03 13:29:06 +1000

    Use unity particleJobSystem

--- Unity/Assets/Scene.cs
+++ Unity/Assets/Scene.cs
@@ -1,29 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
 using UnityEngine.UI;
 
 
 public class Scene : MonoBehaviour
 {
-  float gravity = 0.5f;
+  static float gravity = 0.5f;
+  static Unity.Mathematics.Random rng;
 
   public Text fpsText;
   public Text bunnyText;
 
   public ParticleSystem ps;
-  ParticleSystem.Particle[] m_Particles;
+  private UpdateParticlesJob job = new UpdateParticlesJob();
 
-  float minX = 0;
-  float maxX;
-  float minY = 0;
-  float maxY;
+  static float minX = 0;
+  static float maxX;
+  static float minY = 0;
+  static float maxY;
 
   GameObject bunnyPrefab;
 
   // Start is called before the first frame update
   void Start()
   {
+    rng = new Unity.Mathematics.Random((uint)UnityEngine.Random.Range(int.MinValue, int.MaxValue));
     maxY = -(2f * Camera.main.orthographicSize);
     maxX = 2f * Camera.main.orthographicSize * Camera.main.aspect;
   }
@@ -33,52 +34,6 @@ public class Scene : MonoBehaviour
   {
     fpsText.text = "FPS: " + ((int)(1 / Time.smoothDeltaTime));
 
-    if (m_Particles == null || m_Particles.Length < ps.main.maxParticles)
-    {
-      m_Particles = new ParticleSystem.Particle[ps.main.maxParticles];
-    }
-
-    int numParticles = ps.GetParticles(m_Particles);
-
-    for (int i = 0; i < numParticles; i++)
-    {
-      Vector2 pos = m_Particles[i].position;
-      Vector2 vel = m_Particles[i].velocity;
-
-      vel.y -= gravity;
-
-      if (pos.x > maxX)
-      {
-        vel.x *= -1;
-        pos.x = maxX;
-      }
-      else if (pos.x < minX)
-      {
-        vel.x *= -1;
-        pos.x = minX;
-      }
-
-      if (pos.y < this.maxY)
-      {
-        vel.y *= -0.85f;
-        pos.y = maxY;
-        m_Particles[i].rotation = Random.Range(-6f, 6f);
-        if (Random.Range(0f, 1f) > 0.5f)
-        {
-          vel.y += Random.Range(0f, 6f);
-        }
-      }
-      else if (pos.y > this.minY)
-      {
-        vel.y = 0;
-        pos.y = minY;
-      }
-      m_Particles[i].velocity = vel;
-      m_Particles[i].position = pos;
-
-    }
-    ps.SetParticles(m_Particles, numParticles);
-
     // Add bunnies while over 59fps
     if (1 / Time.smoothDeltaTime > 59)
     {
@@ -86,10 +41,57 @@ public class Scene : MonoBehaviour
       {
         ParticleSystem.EmitParams ep = new ParticleSystem.EmitParams();
         ep.position = new Vector3(0, 0);
-        ep.velocity = new Vector3(Random.Range(0f, 10f), Random.Range(-5f, 5f));
+        ep.velocity = new Vector3((float)rng.NextDouble() * 10f, (float)rng.NextDouble() * 10f - 5f);
         ps.Emit(ep, 1);
       }
       bunnyText.text = "Bunnies: " + ps.particleCount;
+    }
+    job.ScheduleBatch(ps, 2048);
+  }
+
+  struct UpdateParticlesJob : IJobParticleSystemParallelForBatch
+  {
+    public void Execute(ParticleSystemJobData particles, int start, int count)
+    {
+      var positionsX = particles.positions.x;
+      var positionsY = particles.positions.y;
+      var velocitiesX = particles.velocities.x;
+      var velocitiesY = particles.velocities.y;
+      var rotationsZ = particles.rotations.z;
+
+      int end = start + count;
+      for (int i = start; i < end; i++)
+      {
+
+        velocitiesY[i] -= gravity;
+
+        if (positionsX[i] > maxX)
+        {
+          velocitiesX[i] *= -1;
+          positionsX[i] = maxX;
+        }
+        else if (positionsX[i] < minX)
+        {
+          velocitiesX[i] *= -1;
+          positionsX[i] = minX;
+        }
+
+        if (positionsY[i] < maxY)
+        {
+          velocitiesY[i] *= -0.85f;
+          positionsY[i] = maxY;
+          rotationsZ[i] = (float)rng.NextDouble() * 0.2f - 0.1f;
+          if ((float)rng.NextDouble() > 0.5f)
+          {
+            velocitiesY[i] += (float)rng.NextDouble() * 6f;
+          }
+        }
+        else if (positionsY[i] > minY)
+        {
+          velocitiesY[i] = 0;
+          positionsY[i] = minY;
+        }
+      }
     }
   }
 }
